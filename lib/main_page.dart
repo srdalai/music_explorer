86fe57eec506835580a26bae0ebca68050be6ee0
srdalai <satyaranjandalai013@gmail.com>
2018-12-18 20:04:25 +0530

    Added Base Widgets

---
+++ lib/main_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:music_explorer/home_page.dart';
+import 'package:music_explorer/musics_page.dart';
+import 'package:music_explorer/profile_page.dart';
+import 'package:music_explorer/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomePage(),MusicsPage(),SearchPage(),ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _currentIndex,
+        fixedColor: Colors.grey,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Home"),
+              backgroundColor: Colors.grey.shade900),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.music_note),
+              title: Text("Music"),
+              backgroundColor: Colors.grey.shade900),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text("Search"),
+              backgroundColor: Colors.grey.shade900),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              title: Text("Profile"),
+              backgroundColor: Colors.grey.shade900)
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+          _currentIndex = index;
+        });
+  }
+}
---
+++ lib/musics_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MusicsPage extends StatefulWidget {
+  @override
+  _MusicsPageState createState() => _MusicsPageState();
+}
+
+class _MusicsPageState extends State<MusicsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
